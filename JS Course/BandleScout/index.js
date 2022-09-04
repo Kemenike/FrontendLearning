982c1463a44f5d37af25b5f1788c92224c0c865c
kemenike <kanayoemenike37@gmail.com>
2022-09-04 02:03:14 -0700

    Final BandleScout v1.0

--- JS Course/BandleScout/index.js
+++ JS Course/BandleScout/index.js
@@ -1,74 +1,138 @@
-const champCard = document.querySelector(".champion-card");
+let champCards = document.querySelector(".champion-card");
 const champResults = document.querySelector(".champions");
-let champList = [];
+const searchInput = document.querySelector("#search_bar");
+let champList;
+let champListArr = [];
+
+searchInput.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase();
+    champListArr.forEach(champion => {
+        const tags = champion.tags.join(':');
+        const isVisible = champion.name.toLowerCase().includes(value) || tags.toLowerCase().includes(value);
+        document.querySelector(`.${champion.id}`).classList.toggle("hide", !isVisible);
+    })
+})
+
 
 async function getCards() {
-    const result = await fetch ('http://ddragon.leagueoflegends.com/cdn/12.16.1/data/en_US/champion.json');
+    const result = await fetch ('http://ddragon.leagueoflegends.com/cdn/12.16.1/data/en_US/championFull.json');
     await result.json().then(element => {
         champList = element.data;
     });
+    for(let champion in champList) {
+        champListArr.push(champList[champion]);
+    }
     console.log(champList);
-    // console.log(data.data);
-
-    // for(let key in data.data) {
-    //     champResults.innerHTML += cardHTML(data.data[key]);
-    // }
 }
 
 function cardHTML(champion) {
     return `
-    <div class="champion-card">
-        <h3 class="champion-name">
-            ${champion.name} <span class="champion_sub-name"> - ${champion.title}</span>
+    <div class="champion-card ${champion.id} ${champion.tags.join(" ")}">
+    <h3 class="champion-name">
+    ${champion.name} <span class="champion_sub-name"> - ${champion.title}</span>
         </h3>
         <figure>
-            <img src="./assets/champion/${champion.name.replace(/ |\.|\'|\&/g, "")}.png" alt="" class="champion_background">
+            <img src="./assets/champion/${champion.id}.png" alt="" class="champion_background">
         </figure>
-        <div class="details__wrapper">
-                    <div class="health_stat">
-                        <figure class="stat__wrapper">
-                            <img src="./assets//StatMods/StatModsHealthScalingIcon.png" alt="" class="health__icon">
-                        </figure>
-                    </div>
-                    <div class="magic_stat">
-                        <figure class="stat__wrapper">
-                            <img src="./assets/StatMods/StatModsAdaptiveForceIcon.png" alt="" class="health__icon">
-                        </figure>
-                    </div>
-                    <div class="attack_speed_stat">
-                        <figure class="stat__wrapper">
-                            <img src="./assets/StatMods/StatModsAttackSpeedIcon.png" alt="" class="health__icon">
-                            <p class="stat"><span class="stat_per_level"></span></p>
-                        </figure>
-                    </div>
-                    <div class="cooldown_reduction_stat">
-                        <figure class="stat__wrapper">
-                            <img src="./assets/StatMods/StatModsCDRScalingIcon.png" alt="" class="health__icon">
-                            <p class="stat"><span class="stat_per_level"></span></p>
-                        </figure>
-                    </div>
-                    <div class="defence_stat">
-                        <figure class="stat__wrapper">
-                            <img src="./assets/StatMods/StatModsArmorIcon.png" alt="" class="health__icon">
-                            <p class="stat"><span class="stat_per_level"></span></p>
-                        </figure>
-                    </div>
-                    <div class="resistance_stat">
-                        <figure class="stat__wrapper">
-                            <img src="./assets/StatMods/StatModsMagicResIcon.MagicResist_Fix.png" alt="" class="health__icon">
-                        </figure>
-                    </div>
+        <h3 class="detail__title">Stats</h3>
+        <div class="details-stats__wrapper">
+            <div class="stat__wrapper stat__adaptive">
+                <figure>
+                    <img src="./assets/StatMods/StatModsAdaptiveForceIcon.png" alt="" class="stat_icon adaptive__icon">
+                </figure>
+                <div class="adaptive__wrapper">
+                    <p class="stat__info base__ad">${champion.stats.attackdamage} AD <br> <span class="per-level">+${champion.stats.attackdamageperlevel} AD/lvl</span></p>
                 </div>
             </div>
+            <div class="stat__wrapper stat__health">
+                <figure>
+                    <img src="./assets//StatMods/StatModsHealthScalingIcon.png" alt="" class="stat_icon health__icon">
+                </figure>
+                <p class="stat__info">${champion.stats.hp} HP <br> <span class="per-level">+${champion.stats.hpperlevel} HP/lvl</span></p>
+            </div>
+            <div class="stat__wrapper stat__attack-speed">
+                <figure>
+                    <img src="./assets/StatMods/StatModsAttackSpeedIcon.png" alt="" class="stat_icon as__icon">
+                    <p class="stat"><span class="stat_per_level"></span></p>
+                </figure>
+                <p class="stat__info">${champion.stats.attackspeed} AS <br> <span class="per-level">+${champion.stats.attackspeedperlevel} AS/lvl </span></p>
+            </div>
+            <div class="stat__wrapper stat__armor">
+                <figure>
+                    <img src="./assets/StatMods/StatModsArmorIcon.png" alt="" class="stat_icon defense__icon">
+                    <p class="stat"><span class="stat_per_level"></span></p>
+                </figure>
+                <p class="stat__info">${champion.stats.armor} AR <br> <span class="per-level">+${champion.stats.armorperlevel} AR/lvl</span></p>
+            </div>
+            <div class="stat__wrapper stat__resist">
+                <figure>
+                    <img src="./assets/StatMods/StatModsMagicResIcon.MagicResist_Fix.png" alt="" class="stat_icon mr__icon">
+                </figure>
+                <p class="stat__info">${champion.stats.spellblock} MR<br> <span class="per-level">+${champion.stats.spellblockperlevel} MR/lvl</span></p>
+            </div>
+        </div>
+        <h3 class="detail__title">Moves</h3>
+        <div class="move__list">
+            <div class="move__wrapper">
+                <figure class="move__img__wrapper">
+                    <img src="./assets/passive/${champion.passive.image.full}" alt="" class="move__img">
+                </figure>
+            </div>
+            <div class="move__wrapper">
+                <figure class="move__img__wrapper">
+                    <img src="./assets/spell/${champion.spells[0].image.full}" alt="" class="move__img">
+                </figure>
+            </div>
+            <div class="move__wrapper">
+                <figure class="move__img__wrapper">
+                    <img src="./assets/spell/${champion.spells[1].image.full}" alt="" class="move__img">
+                </figure>
+            </div>
+            <div class="move__wrapper">
+                <figure class="move__img__wrapper">
+                    <img src="./assets/spell/${champion.spells[2].image.full}" alt="" class="move__img">
+                </figure>
+            </div>
+            <div class="move__wrapper">
+                <figure class="move__img__wrapper">
+                    <img src="./assets/spell/${champion.spells[3].image.full}" alt="" class="move__img">
+                </figure>
+            </div>
+        </div>
+        <h3 class="role__title">Roles</h3>
+        <div class="role__list">
+            ${showRoles(champion.tags)}
         </div>
     </div>
     `
+}
+
+function getRandomSplash(skinList) {
+    let containerArr = [];
+    for (let skins in skinList) {
+        containerArr.push(skinList[skins].num);
+    }
+    return containerArr[Math.floor(Math.random() * containerArr.length)];
+}
+
+function showRoles(championtags) {
+    if(championtags.length === 1) {
+        return `
+            <p class="role1">${championtags[0]}</p>
+        `;
+    } else {
+        return `
+            <p class="role1">${championtags[0]}, ${championtags[1]}</p>
+        `;
+    }
 }
 
 function renderCards(allChamps, filter) {
     if(!filter) {
         for(let key in allChamps) {
             champResults.innerHTML += cardHTML(allChamps[key]);
+            document.querySelector(`.${allChamps[key].id}`)
+                .style.background = `rgba(0, 0, 0, 0.6) url("../assets/centered/${allChamps[key].id}_${getRandomSplash(allChamps[key].skins)}.jpg") no-repeat center/305%`;
         }
     }
 }
@@ -77,4 +141,4 @@ getCards();
 
 setTimeout(() => {
     renderCards(champList, );
-}, 1000);
+}, 10);
