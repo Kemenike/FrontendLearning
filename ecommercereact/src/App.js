42ee712190e4f77671b1387c2ecfa2f3e5b5a300
kemenike <kanayoemenike37@gmail.com>
2022-09-20 02:31:42 -0700

    Initialize e-commerce project - Home Page Complete

---
+++ ecommercereact/src/App.js
@@ -0,0 +1,24 @@
+import Discounted from "./components/Discounted";
+import Featured from "./components/Featured";
+import Highlights from "./components/Highlights";
+import Landing from "./components/Landing";
+import Nav from "./components/Nav"
+import Explore from "./components/Explore"
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
+      <Explore />
+      <Footer />
+      {/* 50:00 */}
+    </div>
+  );
+}
+
+export default App;
