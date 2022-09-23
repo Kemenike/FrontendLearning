c0c7e8eb8a44917d9024e0169038a1c756d465e1
kemenike <kanayoemenike37@gmail.com>
2022-09-22 23:08:55 -0700

    Final Project

--- ecommercereact/src/App.js
+++ ecommercereact/src/App.js
@@ -6,53 +6,59 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
 import Cart from "./pages/Cart";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function App() {
   const [cart, setCart] = useState([]);
 
-  // function addToCart(book) {
-  //   const dupeItem = cart.find(item => +item.id === +book.id);
-  //   if(dupeItem) { 
-  //     setCart(cart.map(item => {
-  //       if(item.id===dupeItem.id){
-  //         return {
-  //           ...item,
-  //           quantity: item.quantity + 1,
-  //         }
-  //       } else {
-  //         return item;
-  //       }
-  //     }))
-  //   } else {
-  //     setCart([...cart, {...book, quantity: 1}]);
-  //   }
-  // }
-
   function addToCart(book) {
-    setCart([...cart, book]);
+    setCart([...cart, {...book, quantity: 1}]);
   }
 
-  useEffect(() => {
-    console.log(cart);
-  }, [cart]);
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id));
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity;
+    })
+    return counter;
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(cart.map(item => 
+      item.id===book.id
+      ? {
+          ...item,
+          quantity: +quantity,
+      }
+      : item
+    ));
+  }
 
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav numberOfItems={numberOfItems()} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/books" element={<Books books={books} />} />
           <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart} cart={cart} />} />
-          <Route path="/cart" element={<Cart books={books} cart={cart} />} />
+          <Route path="/cart" element={
+            <Cart 
+              books={books} 
+              cart={cart} 
+              changeQuantity={changeQuantity}
+              removeItem={removeItem}
+              />
+          }/>
         </Routes>
         <Footer />
       </div>
     </Router>
   );
 }
-
-2:00:00
 
 export default App;
